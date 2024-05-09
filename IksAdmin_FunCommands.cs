e2ba5d776cd78b7ae4b35fee493dda529c717846
iksixx <pocetikyt@gmail.com>
2024-05-09 21:32:17 +0700

    first commit

---
+++ IksAdmin_FunCommands.cs
@@ -0,0 +1,21 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using IksAdminApi;
+
+namespace IksAdmin_FunCommands;
+
+public class IksAdmin_FunCommands : BasePlugin
+{
+    public override string ModuleName => "IksAdmin_FunCommands";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "iks__ && quiz__";
+
+    private readonly PluginCapability<IIksAdminApi> _pluginCapability = new("iksadmin:core");
+    public static IIksAdminApi? AdminApi;
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        AdminApi = _pluginCapability.Get();
+    }
+
+    
+}
